94694a34fbd9b97aaf17acee5187f0988b9e7497
Merlijn van Lent <merlijn@van-ons.nl>
2024-08-19 16:33:57 +0200

    fix(post-types): Filter post types to include

--- Includes/Settings.php
+++ Includes/Settings.php
@@ -6,7 +6,7 @@ class Settings
 {
     public static function getPostTypes(): array
     {
-        return get_option('vo-html-sitemap-post-types', ['post' => true]);
+        return array_filter( get_option('vo-html-sitemap-post-types', ['post' => true]) );
     }
 
     public static function getPageId(): int
